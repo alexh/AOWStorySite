f48189d260c6be39fd851464690aa29895778097
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-02-15 13:18:07 -0500

    added basic functionality

--- story.js
+++ story.js
@@ -2,3 +2,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $( "#contentWrapper" ).load( "slide1.html");
 });
+
+function Intro(){
+	$( "#contentWrapper" ).load( "Intro.html");
+}
